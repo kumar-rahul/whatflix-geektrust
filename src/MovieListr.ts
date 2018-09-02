4e8060422fc0e80ca63809ee7a1bdd4a7f7beb24
kumarrahul9292 <kumarrahul9292@gmail.com>
2018-09-02 16:03:24 +0530

    dependencies included

--- src/MovieListr.ts
+++ src/MovieListr.ts
@@ -1,34 +1,33 @@
-import * as Koa from "koa";
-import * as bodyParser from "koa-bodyparser";
-import * as logger from "koa-logger";
-import * as Router from "koa-router";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import logger from "koa-logger";
+import Router from "koa-router";
 
 import { createConnection } from "typeorm";
 import { Inject } from "typescript-ioc";
 
-import Director from "./models/Director";
-import Movie from "./models/Movie";
-import Route from "./models/Route";
-import DirectorRoutes from "./routes/DirectorRoutes";
-import MovieRoutes from "./routes/MovieRoutes";
+// import Director from "./models/Director";
+// import Movie from "./models/Movie";
+// import Route from "./models/Route";
+// import DirectorRoutes from "./routes/DirectorRoutes";
+// import MovieRoutes from "./routes/MovieRoutes";
 
 export default class MovieListr {
 
     constructor(
         @Inject private movieRoutes: MovieRoutes,
-        @Inject private directorRoutes: DirectorRoutes) { }
+        @Inject private directorRoutes: DirectorRoutes
+    ) { }
 
     private async createApp() {
         await createConnection({
             name: "default",
-            driver: {
-                type: "mysql",
-                host: "localhost",
-                port: 3306,
-                username: "root",
-                password: "root",
-                database: "movielistr",
-            },
+            type: "mysql",
+            host: "localhost",
+            port: 3306,
+            username: "root",
+            password: "",
+            database: "movielistr",
             entities: [
                 Movie, Director,
             ],
@@ -37,8 +36,8 @@ export default class MovieListr {
         const app: Koa = new Koa();
         const router: Router = new Router();
 
-        this.movieRoutes.register(router);
-        this.directorRoutes.register(router);
+        // this.movieRoutes.register(router);
+        // this.directorRoutes.register(router);
 
         app.use(logger());
         app.use(bodyParser());
